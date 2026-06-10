7ba1073f036bf21ddae1b872b4f69a39dce92667
ghphemanth <hemanthletter@gmail.com>
2026-06-10 23:57:13 +0530

    feat: Add CameraRegistrationService

---
+++ smart-regression-dev-poc/src/main/java/com/app/camera/CameraRegistrationService.java
@@ -0,0 +1,14 @@
+package com.app.camera;
+
+/**
+ * Service for handling camera registration operations
+ */
+public class CameraRegistrationService {
+    
+    /**
+     * Register a new camera
+     */
+    public void registerCamera() {
+        // TODO: Implement camera registration logic
+    }
+}
