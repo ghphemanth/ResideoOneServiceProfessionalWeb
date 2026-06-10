f77fb420b0600b2973c1d53bfd8c06b323bfc0fb
ghphemanth <hemanthletter@gmail.com>
2026-06-11 00:05:58 +0530

    Sync with main: Update CameraDeletionService with implementation

---
+++ smart-regression-dev-poc/src/main/java/com/app/camera/CameraDeletionService.java
@@ -0,0 +1,9 @@
+package com.app.camera;
+
+public class CameraDeletionService {
+
+    public boolean deleteCamera(String cameraName) {
+        System.out.println("Deleting camera: " + cameraName);
+        return true;
+    }
+}
