43023ada9370488ab1ed17ed3c0684ed25820891
ghphemanth <hemanthletter@gmail.com>
2026-06-11 01:54:44 +0530

    Add logging for camera deletion

--- smart-regression-dev-poc/src/main/java/com/app/camera/CameraDeletionService.java
+++ smart-regression-dev-poc/src/main/java/com/app/camera/CameraDeletionService.java
@@ -4,6 +4,7 @@ public class CameraDeletionService {
 
     public boolean deleteCamera(String cameraName) {
         System.out.println("Deleting camera: " + cameraName);
+        
         return true;
     }
 }
