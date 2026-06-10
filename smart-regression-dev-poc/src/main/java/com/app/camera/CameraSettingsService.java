a6bdad91b8e23e0e7c5cc7f9422a9f03c08cf668
ghphemanth <hemanthletter@gmail.com>
2026-06-11 01:56:55 +0530

    Add logging for camera settings update

--- smart-regression-dev-poc/src/main/java/com/app/camera/CameraSettingsService.java
+++ smart-regression-dev-poc/src/main/java/com/app/camera/CameraSettingsService.java
@@ -4,6 +4,7 @@ public class CameraSettingsService {
 
     public boolean updateSettings(String setting) {
         System.out.println("Updating camera setting: " + setting);
+        
         return true;
     }
 }
