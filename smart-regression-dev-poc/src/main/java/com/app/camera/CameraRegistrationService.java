795f9f1c81aa46a849feb3842847336bd5bca9c1
ghphemanth <hemanthletter@gmail.com>
2026-06-11 00:11:03 +0530

    Sync with main: Update CameraRegistrationService with implementation

---
+++ smart-regression-dev-poc/src/main/java/com/app/camera/CameraRegistrationService.java
@@ -0,0 +1,9 @@
+package com.app.camera;
+
+public class CameraRegistrationService {
+
+    public boolean registerCamera(String cameraName) {
+        System.out.println("Registering camera: " + cameraName);
+        return true;
+    }
+}
